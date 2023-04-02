f27a3fef007ef854415bbe8717dd6980283afcc8
MALWICK <batahumphrey66@gmail.com>
2023-04-02 04:06:39 +0100

    creating my logic to calculate time

---
+++ app.js
@@ -0,0 +1,34 @@
+const newYears = "1 Jan 2024";
+
+const dayEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minEl = document.getElementById("minutes");
+const secondsEl = document.getElementById("seconds");
+
+function countDown() {
+  const newYearsDate = new Date(newYears);
+  const currentDate = new Date();
+
+  const totalSeconds = (newYearsDate - currentDate) / 1000;
+
+  const days = Math.floor(totalSeconds / 3600 / 24);
+
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const minutes = Math.floor(totalSeconds / 60) % 60;
+  const seconds = Math.floor(totalSeconds) % 60;
+
+  dayEl.innerHTML = days;
+  hoursEl.innerHTML = formatTime(hours);
+  minEl.innerHTML = formatTime(minutes);
+  secondsEl.innerHTML = formatTime(seconds);
+
+  console.log(days, hours, minutes, seconds);
+}
+
+function formatTime(time) {
+  return time < 10 ? `0${time} ` : time;
+}
+
+countDown();
+
+setInterval(countDown, 1000);
